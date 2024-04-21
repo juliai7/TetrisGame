cbbf980c445ef829d3319f569a9efb5b5b587464
juliaim <julia.im@menloschool.org>
2024-04-20 21:30:30 -0700

    Created classes

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,13 @@
 public class Board {
+    private int height;
+    private int width;
+    private int[][] grid;
+    public Board() {
+        height = 800;
+        width = 500;
+        grid = new int[15][10];
+    }
+    public boolean validPosition(int x, int y) {
+
+    }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,33 @@
 public class Game {
+    private Board board;
+    private GameViewer window;
+    private Tetrominoes currPiece;
+    private int score;
+
+    public Game() {
+        window = new GameViewer(this);
+        score = 0;
+
+    }
+    public void printInstructions() {
+
+    }
+    public void checkCompletion() {
+
+    }
+    //figure out how to get direction input
+    public void movePiece(direction) {
+
+    }
+    public boolean gameOver() {
+
+    }
+    public void playGame() {
+
+    }
+    public static void main(String[] args) {
+        Game newGame = new Game();
+        newGame.playGame();
+    }
 
 }
